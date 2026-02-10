1806ca5565fa60ecb51246232c6b7372a3ccda4c
vegeteria <vegepaad@outlook.com>
2026-02-10 05:52:26 +0000

    hehe

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,25 +1,25 @@
 import Card from "../Card"
+import searchTrack from "../../lib/Spotify"
 
 
-export default async function SearchPage({searchParams}){
-    const {query} = await searchParams || ""
+export default async function SearchPage({ searchParams }) {
+    const { query } = await searchParams || ""
     // console.log(query)
-    const res = await fetch(`${process.env.BASE_URL}/api/search?query=${encodeURIComponent(query)}`,{cache: "no-store"})
-    const results = await res.json()
+    const results = await searchTrack(query)
     // console.log(results)
-    return(
+    return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-16 mx-8 max-sm:mx-2">
-        {results.map((item)=>(
-            <Card 
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              artist={item.artist}
-              album={item.album}
-              url={item.url}
-              artwork={item.artwork}
-            />
-        ))}
+            {results.map((item) => (
+                <Card
+                    key={item.id}
+                    id={item.id}
+                    name={item.name}
+                    artist={item.artist}
+                    album={item.album}
+                    url={item.url}
+                    artwork={item.artwork}
+                />
+            ))}
         </div>
     )
 }
